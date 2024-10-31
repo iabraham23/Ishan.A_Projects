0dfa6f3d0b3b683aaeada547cadbfcf640d82eb8
iabraham23 <ishansport@gmail.com>
2024-10-31 10:44:36 -0700

    Update morse_code.py

--- Projects/morse_code.py
+++ Projects/morse_code.py
@@ -2,32 +2,27 @@
 RULES
 
 * a dot lasts for one unit
-* a dash last for three units
+* a dash last for three unit
 
 * the space between dots and dashes that are part of the same letter is one unit
 * the space between different letters is three units
 * the space between different words is seven units
 
-
-text_to_morse: displays the morse code with correct timings between letters and words 
-no_spacing: displays raw morse code with no spacing, used for displaying on webpage  
+text_to_morse: translates text to morse code with correct spacing
+no_spacing(text_to_morse): gives morse code without spacing, used for display on webpage
 """
 
 def morse_dict():
-    
     #codebook = {chr(x): [] for x in range(ord('A'), ord('Z')+1)}
     #return codebook
-    
     codebook = {'A': ['*', '-'], 'B': ['-', '*', '*', '*'], 'C': ['-', '*', '-', '*'],
                 'D': ['-', '*', '*'], 'E': ['*'], 'F': ['*', '*', '-', '*'], 'G': ['-', '-', '*'],
-                'H': ['*', '*', '*', '*'], 'I': ['*', '*'], 'J': ['*', '-', '-', '-'], 
-                'K': ['-', '*', '-'],'L': ['*', '-', '*', '*'], 'M': ['-', '-'], 'N': ['-', '*'], 
-                'O': ['-', '-', '-'], 'P': ['*', '-', '-', '*'],'Q': ['-', '-', '*', '-'], 
-                'R': ['*', '-', '*'], 'S': ['*', '*', '*'], 'T': ['-'], 'U': ['*', '*', '-'],
+                'H': ['*', '*', '*', '*'], 'I': ['*', '*'], 'J': ['*', '-', '-', '-'], 'K': ['-', '*', '-'],
+                'L': ['*', '-', '*', '*'], 'M': ['-', '-'], 'N': ['-', '*'], 'O': ['-', '-', '-'], 'P': ['*', '-', '-', '*'],
+                'Q': ['-', '-', '*', '-'], 'R': ['*', '-', '*'], 'S': ['*', '*', '*'], 'T': ['-'], 'U': ['*', '*', '-'],
                 'V': ['*', '*', '*', '-'], 'W': ['*', '-', '-'], 'X': ['-', '*', '*', '-'],
                 'Y': ['-', '*', '-', '-'], 'Z': ['-', '-', '*', '*']}
     return codebook
-
 
 
 def word_to_morse(string):
@@ -36,6 +31,8 @@ def word_to_morse(string):
     upper_str = string.upper()
 
     for letter in upper_str:
+        if letter not in codebook:
+            return f' Cannot translate to morse, unknown letter {letter}'
         x = codebook[letter]
         code+='2'
 
@@ -48,8 +45,28 @@ def text_to_morse(text):
     translate = ''
 
     for word in text.split():
-        translate += word_to_morse(word) +'4'
-    return translate[1:-2] #gets rid of leading 1, final 1 and 4
+        translate+=(word_to_morse(word) +'4')
+
+    translate = translate[1:-2] #gets rid of leading 1, final 1 and 4
+    cleaned = []
+
+    val = 0
+    while val <= len(translate)-1: #summing consecutive numbers, getting correct spacing
+        if translate[val].isdigit() and translate[val+1].isdigit():
+            if translate[val +2].isdigit():
+
+                x = int(translate[val]) + int(translate[val+1]) + int(translate[val+2])
+                cleaned.append(x)
+                val +=3
+            else:
+                cleaned.append(int(translate[val]) + int(translate[val+1]))
+                val+=2
+        else:
+            cleaned.append(translate[val])
+            val+=1
+
+    cleaned = ''.join(str(x) for x in cleaned)
+    return cleaned
 
 def no_spacing(morse):
     result = ''
